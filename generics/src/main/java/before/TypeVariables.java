680899edcb20bd69e4a26d71f8f626f90c7a7722
muratslavich <slavichmo@gmail.com>
2021-02-27 14:35:11 +0300

    add generics example

---
+++ generics/src/main/java/before/TypeVariables.java
@@ -0,0 +1,18 @@
+package before;
+
+import java.util.Collection;
+
+public class TypeVariables {
+
+    // type variable in declaring a class or method - <T extends Object & Comparable<T>>
+    // could be bounded only on top (extends) - <T extends Comparable<T>>
+    // multiple bounds - left bound is used for type erasure
+    public static <T extends Object & Comparable<T>> T max(Collection<T> coll) {
+        T candidate = coll.iterator().next();
+        for (T elt : coll) {
+            if (candidate.compareTo(elt) < 0) candidate = elt;
+        }
+        return candidate;
+    }
+
+}
---
+++ generics/src/main/java/before/Wildcards.java
@@ -0,0 +1,66 @@
+package before;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/*
+* Animal -> Cat
+*
+* Covariance - keep original hierarchy - Set<Animal> = Set<Cat>
+* Contravariance - inverting hierarchy in derived types - Set<Cat> = Set<Animal>
+* Invariance - no inheritance between derived types -  Set<Animal> != Set<Cat>
+* */
+
+public class Wildcards {
+
+
+
+    private static class Animal { }
+
+    private static class Cat extends Animal {  }
+    private static class Dog extends Animal {  }
+
+
+    public static void main(String[] args) {
+
+        // Arrays in Java are covariant
+        Animal[] animals = new Cat[10];
+        animals[0] = new Cat();
+        animals[1] = new Dog(); // ArrayStoreException in runtime
+
+        // Generics are invariant
+        // 1 example
+        List<Animal> animalList = new ArrayList<Dog>(); // compile time error
+        // 2 example
+        List<Animal> animalList1;
+        List<Cat> cats = List.of(new Cat[2]);
+        animalList1 = cats; // compile time error
+        // 3 example
+        List<Integer> ints = Arrays.asList(1,2,3);
+        List<Number> nums = ints; // compile time error
+
+        // Generics can be covariant with wildcard
+        // PECS
+        // producer - only get
+        List<? extends Animal> animalList2 = new ArrayList<Dog>();
+        animalList2.add(new Dog()); // compile time error
+        animalList2.add(null);
+        Animal animal = animalList2.get(0);
+
+        // And contravariance
+        // consumer - only add
+        List<? super Dog> animalList3 = new ArrayList<Animal>();
+        animalList3.add(new Dog());
+        Animal animal1 = animalList3.get(0); // compile time error
+        Object object = animalList3.get(0);
+
+        // Raw type = ? extend Object
+        List<?> animalList4 = new ArrayList<>();
+        animalList4.add(new Dog());
+        animalList4.add(null);
+        Object o = animalList4.get(0);
+
+    }
+}
