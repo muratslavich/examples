213580888c43f54dcc8a3b55774d5d8db4bfea7d
muratslavich <slavichmo@gmail.com>
2023-01-10 23:40:29 +0300

    add upload example

--- upload-and-retrieve-images-sse/src/main/kotlin/com/prot/example/Main.kt
+++ upload-and-retrieve-images-sse/src/main/kotlin/com/prot/example/Main.kt
@@ -3,6 +3,8 @@ package com.prot.example
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.util.FileCopyUtils
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
@@ -10,6 +12,7 @@ import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBo
 import java.io.File
 import java.io.FileInputStream
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 import java.nio.file.StandardOpenOption
 import java.util.*
@@ -21,11 +24,9 @@ fun main() {
     runApplication<Main>()
 }
 
-const val UPLOAD_DIRECTORY = "/images/upload"
-
 @RestController
 class ImageController(
-    @Value("\${data.dir}") val absolutePath: String
+    val dataPath: Path,
 ) {
 
     @PostMapping("/image")
@@ -33,7 +34,7 @@ class ImageController(
         @RequestParam payload: MultipartFile
     ): String {
         val uid = UUID.randomUUID().toString()
-        val path = Paths.get(absolutePath + UPLOAD_DIRECTORY, uid)
+        val path = Paths.get(dataPath.toString(), uid)
         Files.write(path, payload.bytes, StandardOpenOption.CREATE_NEW)
         return uid
     }
@@ -42,10 +43,21 @@ class ImageController(
     fun getImage(
         @PathVariable uid: String
     ): StreamingResponseBody {
-        val path = Paths.get(absolutePath + UPLOAD_DIRECTORY, uid)
+        val path = Paths.get(dataPath.toString(), uid)
         val fileInputStream = FileInputStream(File(path.toUri()))
         return StreamingResponseBody {
             FileCopyUtils.copy(fileInputStream, it)
         }
     }
+}
+
+const val UPLOAD_DIRECTORY = "/images/upload"
+
+@Configuration
+class Config {
+    @Bean
+    fun checkDirectories(@Value("\${data.dir}") absolutePath: String): Path =
+        Files.createDirectories(Paths.get(absolutePath + UPLOAD_DIRECTORY))
+
+
 }
